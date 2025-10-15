83e2f43214f416d66366aa7940c3a41965628bdb
Semen Tonkikh <tonksemen@gmail.com>
2025-10-15 14:05:25 +0300

    add swap_caps.sh and map that to super + n

---
+++ .scripts/swap_caps.sh
@@ -0,0 +1,23 @@
+CONFIG="$HOME/.config/hypr/input.conf"
+STATE_FILE="$HOME/.cache/swap_caps_state"
+
+OPT1="kb_options = grp:win_space_toggle"
+OPT2="kb_options = grp:win_space_toggle, ctrl:swapcaps"
+
+if [ -f "$STATE_FILE" ]; then
+  STATE=$(cat "$STATE_FILE")
+else
+  STATE=0
+fi
+
+if [ "$STATE" -eq 0 ]; then
+  sed -i "s|^[[:space:]]*kb_options *=.*|  $OPT2|" "$CONFIG"
+  echo 1 > "$STATE_FILE"
+  notify-send "󰪛  Caps Lock" "Caps Lock SWAPPED with ctrl"
+else
+  sed -i "s|^[[:space:]]*kb_options *=.*|  $OPT1|" "$CONFIG"
+  echo 0 > "$STATE_FILE"
+  notify-send "󰪛  Caps Lock" "Caps Lock DOES NOT swapped with ctrl"
+fi
+
+hyprctl reload > /dev/null
