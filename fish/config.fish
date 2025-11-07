cccf386baa1445bf2f587711c2ab4cd41ed93c4f
Semen Tonkikh <tonksemen@gmail.com>
2025-11-07 15:58:51 +0300

    upd ssh kys

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set -gx PATH /usr/bin $PATH
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 if not ssh-add -l >/dev/null 2>&1
     ssh-add ~/.ssh/id_ed25519_bs >/dev/null 2>&1
+    ssh-add ~/.ssh/id_ed25519-the-simen >/dev/null 2>&1
 end
 
 function fish_user_key_bindings
